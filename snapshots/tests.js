353dcd7fe07bdfe4a2f87b503b5301f1470144f8
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-04-23 20:31:49 -0600

    update snapshots

--- snapshots/tests.js
+++ snapshots/tests.js
@@ -1,3 +1,13 @@
 import {} from "./scratch2.js"
 import {} from "./scratch3.js"
 import {} from "./snap.js"
+
+// import { test } from "./runner.js";
+// 
+// test(
+//     "snap",
+//     "en-custom",
+//     `
+//     <custom block :: operators> :: define+
+//     `
+//   )
