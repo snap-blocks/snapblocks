12100d6b6a7ab49319b7ea9a8c77a0a0399b84ea
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-08-08 21:07:28 -0600

    add local pin
    I also made newlines start at the same x position as the first part after the line in extension blocks (or local blocks).

--- scratch3/blocks.js
+++ scratch3/blocks.js
@@ -694,12 +694,20 @@ class BlockView {
     ) {
       this.info.category = categoryAliases[this.info.category]
     }
-    if (Object.prototype.hasOwnProperty.call(extensions, this.info.category)) {
+    if (
+      Object.prototype.hasOwnProperty.call(extensions, this.info.category) ||
+      this.info.local
+    ) {
       this.children.unshift(new LineView())
-      this.children.unshift(
-        new IconView({ name: this.info.category + "Block" }),
-      )
-      this.info.category = "extension"
+      if (Object.prototype.hasOwnProperty.call(extensions, this.info.category)) {
+        this.children.unshift(
+          new IconView({ name: this.info.category + "Block" }),
+        )
+        this.info.category = "extension"
+      }
+      if (this.info.local) {
+        this.children.unshift(newView(new Icon('location', 1.5)))
+      }
     }
 
     this.x = 0
@@ -996,11 +1004,12 @@ class BlockView {
       ptmin = padding[0][0],
       pbmin = padding[1][0]
 
-    let y = this.info.shape === "cat" ? 16 : 0
+    let y = this.info.shape === "cat" ? 16 : 0    
+    let extensionLineWidth = 0
     class Line {
       constructor(y) {
         this.y = y
-        this.width = 0
+        this.width = extensionLineWidth
         this.padding = {
           top: 0,
           bottom: 0,
@@ -1089,6 +1098,10 @@ class BlockView {
       }
 
       child.el = child.draw(options, this)
+
+      if (previousChild?.isLine) {
+        extensionLineWidth = line.width + this.marginBetween(previousChild, child)
+      }
 
       if (child.isCShape) {
         if (noWrapLine.length > 0) {
@@ -1235,7 +1248,7 @@ class BlockView {
         if (child.isIcon && i === 0 && this.isCommand) {
           line.height = Math.max(line.height, child.height)
         }
-
+        
         child.x = line.width
         line.width += child.width
         innerWidth = Math.max(innerWidth, line.width)
@@ -1282,6 +1295,7 @@ class BlockView {
 
     let padLeft = children.length ? this.horizontalPadding(children[0]) : 0
     const padRight = children.length ? this.horizontalPadding(lastChild) : 0
+    
     innerWidth += padLeft + padRight
 
     // Commands have a minimum width.
