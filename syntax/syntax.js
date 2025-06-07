20ca45ecc5a419803cac21af461bedff5b42620d
Benjamin Curry <70973578+ego-lay-atman-bay@users.noreply.github.com>
2025-06-07 16:15:31 -0600

    fix showing `undefined` at the end of comment line after \

--- syntax/syntax.js
+++ syntax/syntax.js
@@ -1339,9 +1339,9 @@ function parseLines(code, languages) {
       raw += tok
       if (tok === "\\") {
         next()
-        raw += tok
+        raw += tok ? tok : ''
       }
-      comment += tok
+      comment += tok ? tok : ''
       next()
     }
     if (isMultiline && tok && tok === "*") {
